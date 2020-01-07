edad5ec8e36c634b5f9ebda6855c6f960a5fe787
Jamie <jamiekaram16@gmail.com>
2020-01-07 21:52:56 +0000

    Couple of INSERTS

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,48 @@
+
+    INSERT INTO users (
+    name, email, password) 
+    VALUES (
+    'Devin Sanders', 'tristanjacobs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+    INSERT INTO users (
+    name, email, password) 
+    VALUES (
+    'Iva Harrison', 'allisonjackson@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+    INSERT INTO users (
+    name, email, password) 
+    VALUES (
+    'Lloyd Jefferson', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+        INSERT INTO properties (
+    title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, active, province, city, country, street, post_code) 
+    VALUES (
+    'Expect orbit', 'Arm trade hurried allow club knife wave catch however under time straight away tune twelve sent middle sort shelter somehow three thus involved simplest firm small breath music mathematics fly yellow street were tail goose plus principal interest chest think what begun green decide difference tea hurry advice be seems certainly without wool perhaps angry way page my walk test telephone take tonight busy foot court iron people barn scientist slave rays cheese aloud feathers average fish burst saved forgot camp rate whatever monkey distance closely missing post scientist out football noun fighting peace gravity weak live fur repeat hurried gun wherever desert current twice hurt describe lonely surface substance sweet but problem result seems keep behind with tide community ill nearly scene meal music duty easier possible together harbor held frame fifteen suggest twice village older fairly pour headed ancient never tape rubbed swimming quickly piece row start fallen milk popular shake brush according adventure failed ocean glass single farther forward farther this brown changing later swung image union cookies provide her driving blanket fair recent frog individual neighbor public direction meant none getting parallel recall better ice eager temperature dug accurate discover allow bare broke arm practice original lot thank birthday scientist about agree section impossible change bent passage finish bad tube greatly money everything available cake shape airplane sight shall strange refer rather seldom nor like mostly separate plane prove whale leather whose part best declared expression order during birth further high earn load plural hold food fastened ground inside sad all sets recognize should simple tonight terrible gasoline riding behind record baby dream difference using shine yes thee loud it sentence class nothing mud without represent instrument middle village market round earth men member melted spend environment properly sign interest escape meal completely consist movement put', 136, 'https://images.pexels.com/photos/463996/pexels-photo-463996.jpeg', 'https://images.pexels.com/photos/463996/pexels-photo-463996.jpeg?auto=compress&cs=tinysrgb&h=350', 40391, 2, 1, 9, true, 'British Columbia', 'North Cowichan', 'Canada', '230 Musam Glen', '75442');
+    INSERT INTO properties (
+    title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, active, province, city, country, street, post_code) 
+    VALUES (
+    'North wrong', 'Weak practice master type however black breeze touch feel but exactly so wise pride raw burn colony fought glad date matter harbor shaking easier member weather run mathematics quickly pictured circus event seven pan pool journey southern mix recall desert both construction by told produce combine proud edge lungs zulu piano himself halfway common ever reader worry alone thou door reach relationship make sides universe leg grew kept coming low fly nearer wear town represent them soldier against about hung mission surprise father share generally active part horn weight minerals horse harbor fish soldier wish eaten listen government according drive egg hungry fill nature upon death case salmon location officer upon fed teacher hurry went balance contain bush possible your on cow lift castle straight independent ride threw independent musical porch peace group foot caught visitor nuts interior command recall although rest whenever dog hunter wealth pencil general ago at cause chicken failed different cat wolf fell bear safety motor sweet captured closely pressure became sister aside wagon throughout start never hungry area sentence ear tried enter likely failed closer might cotton driver music again bicycle nearby flew almost law carbon class arrangement vast quarter call broke meal stood deer amount however familiar clay fight college came policeman substance children signal when horse thirty bottom plenty lost start rope protection today carbon species twelve bar current year expect food eaten charge tight left necessary familiar sunlight tiny top colony fifty bee meet knowledge independent terrible plan job strip body felt slowly shown mood brown musical explain ahead heart fighting sold success avoid air distance captain tin heavy correctly morning catch brain neighborhood load fairly machine unhappy desert slide donkey birthday army feed serious nearer machinery dried offer even fifth paragraph gentle finest manufacturing indicate bowl gather hung heavy soil organization', 449, 'https://images.pexels.com/photos/221540/pexels-photo-221540.jpeg', 'https://images.pexels.com/photos/221540/pexels-photo-221540.jpeg?auto=compress&cs=tinysrgb&h=350', 83150, 4, 0, 6, true, 'Newfoundland And Labrador', 'Corner Brook', 'Canada', '77 Diwdas Key', '89806');
+
+    INSERT INTO reservations (
+    id, guest_id, property_id, start_date, end_date) 
+    VALUES (
+    1009999, 743, 114, '2022-09-30', '2022-10-15');
+    INSERT INTO reservations (
+    id, guest_id, property_id, start_date, end_date) 
+    VALUES (
+    1010000, 269, 841, '2021-04-30', '2021-05-18');
+
+    INSERT INTO property_reviews (
+    guest_id, property_id, reservation_id, rating, message) 
+    VALUES (
+    328, 190, 1015655, 4, 'Sigkez amafozuz zidak cirazke giikeak lazup cefumzeb ohefcob vugsi vudavi ahevi awogi tagjowos.');
+    INSERT INTO property_reviews (
+    guest_id, property_id, reservation_id, rating, message) 
+    VALUES (
+    340, 769, 1013720, 5, 'Filin alforec pad sevopa ej jesebfuh tap ma ajafihjoc pujawoso kuez zitu sebfal zialuti jool rudpi gogofiri da.');
+    INSERT INTO property_reviews (
+    guest_id, property_id, reservation_id, rating, message) 
+    VALUES (
+    891, 290, 1016559, 3, 'Hawapel pasale udmupnej co uzfos novuhig bilad dedgure vim renfef soumi suti bop.');
+    INSERT INTO property_reviews (
+    guest_id, property_id, reservation_id, rating, message) 
+    VALUES (
+    831, 195, 1011943, 3, 'Mu ozconas cuusoese kifefake cevletal gi fafsisar neza pafvum fepuje felsi tevcu nonu fa nod vukpoaj raw.');
