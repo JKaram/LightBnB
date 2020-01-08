aeded6119ad7eb12f4c3dc9f3eab0b95aa2d2d60
Jamie <jamiekaram16@gmail.com>
2020-01-08 22:56:29 +0000

    Add property

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -83,7 +83,7 @@ exports.addUser = addUser;
  */
 const getAllReservations = function(guest_id, limit = 10) {
   return pool.query(`
-  SELECT properties.*  FROM reservations 
+  SELECT DISTINCT properties.*  FROM reservations 
   JOIN property_reviews ON reservations.id = property_reviews.reservation_id
   JOIN properties ON properties.id = property_reviews.property_id
   WHERE reservations.guest_id = $1
@@ -104,14 +104,74 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
+  console.log('OPTIONS', options)
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  
+  `;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night);
+    if (options.city) {
+    queryString += `AND properties.cost_per_night > $${queryParams.length} `;
+    } else {
+      queryString += `WHERE properties.cost_per_night  > $${queryParams.length} `;
+    }
+  }
+
+
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night);
+    if (options.city || options.minimum_price_per_night) {
+    queryString += `AND properties.cost_per_night < $${queryParams.length} `;
+    } else {
+      queryString += `WHERE properties.cost_per_night  < $${queryParams.length} `;
+    }
+  }
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    if (options.city || options.minimum_price_per_night || options.maximum_price_per_night) {
+    queryString += `HAVING average_rating > $${queryParams.length} `;
+    } else {
+      queryString += `WHERE properties.average_rating  > $${queryParams.length} `;
+    }
+  }
+  // 4
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams)
   .then(res => res.rows)
-}
+};
 exports.getAllProperties = getAllProperties;
 
+// SELECT properties.id, properties.title, properties.cost_per_night, avg(property_reviews.rating) FROM properties
+// JOIN property_reviews ON property_reviews.property_id = properties.id
+// GROUP BY properties.id, properties.title, properties.cost_per_night
+// HAVING avg(property_reviews.rating) >= 4 AND properties.city LIKE '%Vancouver%'
+// ORDER BY properties.cost_per_night
+// LIMIT 10;
 
 /**
  * Add a property to the database
@@ -119,9 +179,14 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  console.log(property)
+  return pool.query(`
+  INSERT INTO properties (
+    title, description, thumbnail_photo_url, cover_photo_url,  cost_per_night, street, city,  province, post_code,  country, parking_spaces, number_of_bathrooms, number_of_bedrooms) 
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;
+  `, [property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
+    .then(res => 
+      res.rows[0]
+    )
 }
 exports.addProperty = addProperty;
